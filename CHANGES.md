7c242d068c0862041ac99722206d0cf5b644a994
root <root@lab-05.orangutan.local>
2018-01-30 14:06:02 -0600

    fixed node joins, added CHANGES.md, .gitignore, and LICENSE

---
+++ CHANGES.md
@@ -0,0 +1,7 @@
+# Changelog (KubernetesHomeLab)
+
+## 2018-01-30
+
+* Fixed (master & nodes: hosts file injection
+* Added ```.gitignore```
+* Fixed (nodes): ```misconfiguration: kubelet cgroup driver: "systemd" is different from docker cgroup drive```
--- master.sh
+++ master.sh
@@ -4,10 +4,12 @@ swapoff -a
 dnf -y install dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 dnf install -y docker-ce-17.03.0.ce-1.fc25
+sed -i "s|ExecStart=/usr/bin/dockerd|ExecStart=/usr/bin/dockerd --exec-opt native.cgroupdriver=cgroupfs|" /lib/systemd/system/docker.service
+systemctl daemon-reload
 systemctl enable docker.service
 systemctl start docker
 
-echo /vagrant/hsost >> /etc/hosts
+cat /vagrant/hosts | tee -a /etc/hosts
 setenforce 0
 cp /vagrant/kubernetes.repo /etc/yum.repos.d/
 yum install -y kubelet kubeadm kubectl
